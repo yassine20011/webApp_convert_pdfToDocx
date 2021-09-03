040663297840190f23f6033f3b50825d062ddeb9
YASSINE AMJAD <59234686+yassine20011@users.noreply.github.com>
2021-09-03 19:58:25 +0100

    update

--- projectConvert/settings.py
+++ projectConvert/settings.py
@@ -124,5 +124,5 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, '/dev/sda1/media/')
 MEDIA_URL = '/dev/sda1/media/'
