af6cd5c0961525aa342e7c104912558a2012515b
yassine20011 <medalgamer47@gmail.com>
2021-09-22 19:11:41 +0100

    Updated

--- projectConvert/settings.py
+++ projectConvert/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
+MEDIA_URL = 'media/'
 
 if DEBUG:
 
@@ -139,4 +139,4 @@ else:
 
   STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
