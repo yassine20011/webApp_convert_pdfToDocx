9d3493921738f490c9cefa21df9b80cc093ceb10
yassine20011 <medalgamer47@gmail.com>
2021-10-06 23:24:48 +0100

    Updated ALLOWED HOSTS

--- projectConvert/settings.py
+++ projectConvert/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '@wo$r)7w!=u0_uc2o$t8-!k(nsj1n4r*-7ym6qfp!7b+c&ijdm'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '52.45.165.238', 'www.freeconvert.tk', 'freeconvert.tk']
+ALLOWED_HOSTS = ['m.freeconvert.tk','127.0.0.1', '52.45.165.238', 'www.freeconvert.tk', 'freeconvert.tk']
 
 # Application definition
 
