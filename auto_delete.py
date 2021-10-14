d6a2bc159dfed3d0a0ffbaa035f14577d38c4fe7
yassine20011 <medalgamer47@gmail.com>
2021-10-14 16:44:13 +0100

    Updated

---
+++ auto_delete.py
@@ -0,0 +1,24 @@
+import time
+import os
+
+
+def delete():
+    while True:
+        time.sleep(60*60)
+        if len(os.listdir(r'C:\Users\AMJAD\Desktop\test00\yassine\media')) == 0:
+            print("Directory is empty")
+        else:    
+            print("Directory is not empty")
+            counter = 1
+            print(counter)
+            control_while = True
+            while control_while:
+                time.sleep(1)
+                counter += 1
+                print(counter)
+                if counter == 5:
+                    os.system(".\\rm.sh")
+                    control_while = False
+                else:
+                    control_while = True
+delete()
